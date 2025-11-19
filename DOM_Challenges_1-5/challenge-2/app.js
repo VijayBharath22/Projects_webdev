bb961729ba386cd4cb65dc32ad019e8017b77d17
VijayBharath22 <vb222006@gmail.com>
2025-11-19 12:56:14 +0530

    challenge 2 completed

--- DOM_Challenges_1-5/challenge-2/app.js
+++ DOM_Challenges_1-5/challenge-2/app.js
@@ -1,3 +1,9 @@
-/**
- * Write your challenge solution here
- */
+let btns = document.querySelector(".color-buttons");
+console.log(btns);
+let changebtn = document.getElementById("mainHeading");
+
+btns.addEventListener("click", (e) => {
+  if (e.target.tagName == "BUTTON") {
+    changebtn.style.color = e.target.innerHTML;
+  }
+});
