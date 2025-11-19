a78a4fba50253f28362b2ca410b8ef109424484d
VijayBharath22 <vb222006@gmail.com>
2025-11-19 12:14:24 +0530

    challenge 1 finished

--- DOM_Challenges_1-5/challenge-1/app.js
+++ DOM_Challenges_1-5/challenge-1/app.js
@@ -1,3 +1,17 @@
-/**
- * Write your challenge solution here
- */
+let btn = document.getElementById("toggleButton");
+let bulb = document.getElementById("bulb");
+let pagestatus = document.getElementById("status");
+
+btn.addEventListener("click", function () {
+  if (document.body.classList.contains("dark-mode")) {
+    document.body.classList.remove("dark-mode");
+    pagestatus.innerHTML = "status: Off";
+    btn.innerHTML = "Turn On";
+    bulb.classList.add("off");
+  } else {
+    btn.innerHTML = "Turn Off";
+    document.body.classList.add("dark-mode");
+    pagestatus.innerHTML = "status: On";
+    bulb.classList.remove("off");
+  }
+});
