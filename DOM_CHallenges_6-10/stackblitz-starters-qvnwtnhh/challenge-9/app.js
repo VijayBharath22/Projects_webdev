75bc19f887814dd3f3e9b0a8a726e5a6ae7cd298
VijayBharath22 <vb222006@gmail.com>
2026-02-08 09:53:25 +0530

    challenge 9 completed

--- DOM_CHallenges_6-10/stackblitz-starters-qvnwtnhh/challenge-9/app.js
+++ DOM_CHallenges_6-10/stackblitz-starters-qvnwtnhh/challenge-9/app.js
@@ -1,3 +1,15 @@
-/**
- * Write your challenge solution here
- */
+let toggleBtn = document.querySelector(".toggle-btn");
+let panel = document.querySelector(".panel");
+let closeBtn = document.querySelector(".close-btn");
+
+toggleBtn.addEventListener("click", (e) => {
+  panel.classList.toggle("active");
+});
+
+closeBtn.addEventListener("click", () => panel.classList.remove("active"));
+
+panel.addEventListener("click", (e) => {
+  let item = e.target;
+  if (e.target.classList == "menu-item")
+    alert(`You opened ${item.textContent} `);
+});
